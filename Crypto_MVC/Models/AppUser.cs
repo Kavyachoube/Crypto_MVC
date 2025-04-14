06d66af1c73124d6d376abb7f1793aa0d6d3825e
Kavya Choubey <kavya.choube@gmail.com>
2025-04-14 13:40:50 +0530

    login for admin

---
+++ Crypto_MVC/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Crypto_MVC.Models
+{
+    public class AppUser
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
